237e134d653ea828ef6cd8cf38cf8f7ab0f7cc6b
abadiHariri <a.hariri@salla.sa>
2024-02-01 15:15:16 +0300

    fix

--- Sources/Network/NetworkDetailViewController.swift
+++ Sources/Network/NetworkDetailViewController.swift
@@ -427,8 +427,10 @@ extension NetworkDetailViewController {
         
         //2.click edit view
         cell.tapEditViewCallback = { [weak self] detailModel in
-             let previewView = JSONPreview()
-            previewView.preview(detailModel?.content ?? "")
+            let vc = BasicExampleViewController()
+//
+//             let previewView = JSONPreview()
+//            previewView.preview(detailModel?.content ?? "")
             vc.json = detailModel?.content ?? ""
             //let vc = JsonViewController.instanceFromStoryBoard()
            // vc.detailModel = detailModel
